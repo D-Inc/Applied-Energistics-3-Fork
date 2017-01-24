33e0271ed5c38a113343dda0164ac3a181715b9c
elix-x <Elix-x@users.noreply.github.com>
2017-01-24 21:15:50 +0100

    Added a bridge method from material definition to material item subdefinition

--- src/api/java/appeng/api/definitions/IMaterialDefinition.java
+++ src/api/java/appeng/api/definitions/IMaterialDefinition.java
@@ -1,8 +1,18 @@
+
 package appeng.api.definitions;
 
+
+import net.minecraft.item.Item;
+
+import appeng.api.definitions.sub.ISubDefinition;
+import appeng.api.definitions.sub.ISubDefinitionProperty;
+import appeng.core.api.items.IItemMaterial;
 import appeng.core.item.material.Material;
+
 
 public interface IMaterialDefinition<M extends Material> extends IDefinition<M>
 {
+
+	public <I extends Item & IItemMaterial<I>, D, P extends ISubDefinitionProperty<I, D, ?>, S extends ISubDefinition<I, D, P, ? extends S>> S asSubDefinition();
 
 }
