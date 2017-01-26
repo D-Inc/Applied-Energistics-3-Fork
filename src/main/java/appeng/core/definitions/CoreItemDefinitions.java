34a87d38e2eaf3d3ecaa0a68fb3a71eb861b5ebf
elix-x <Elix-x@users.noreply.github.com>
2017-01-26 19:41:06 +0100

    Implemented registration of material item

--- src/main/java/appeng/core/definitions/CoreItemDefinitions.java
+++ src/main/java/appeng/core/definitions/CoreItemDefinitions.java
@@ -8,9 +8,9 @@ import net.minecraft.util.ResourceLocation;
 import appeng.api.definitions.IItemDefinition;
 import appeng.core.AppEng;
 import appeng.core.api.definitions.ICoreItemDefinitions;
-import appeng.core.item.ItemCrystalSeed;
 import appeng.core.client.render.ItemCrystalSeedRendering;
-import appeng.core.item.ItemMultiItem;
+import appeng.core.item.ItemCrystalSeed;
+import appeng.core.item.material.ItemMaterial;
 import appeng.core.lib.bootstrap.FeatureFactory;
 import appeng.core.lib.definitions.Definitions;
 
@@ -19,12 +19,12 @@ public class CoreItemDefinitions extends Definitions<Item, IItemDefinition<Item>
 {
 
 	private final IItemDefinition crystalSeed;
-	private final IItemDefinition multiItem;
+	private final IItemDefinition materialItem;
 
 	public CoreItemDefinitions( FeatureFactory registry )
 	{
 		this.crystalSeed = registry.item( new ResourceLocation( AppEng.NAME, "crystal_seed" ), new ItemCrystalSeed() ).rendering( new ItemCrystalSeedRendering() ).build();
-		this.multiItem = registry.item( new ResourceLocation( AppEng.NAME, "multi_tem" ), new ItemMultiItem() ).build();
+		this.materialItem = registry.item( new ResourceLocation( AppEng.MODID, "material" ), new ItemMaterial() ).build();
 		init( registry.buildDefaultItemBlocks() );
 	}
 
