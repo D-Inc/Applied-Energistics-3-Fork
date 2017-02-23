1b3dc64f32ff99209248273ec0257cb9e59556dc
elix-x <Elix-x@users.noreply.github.com>
2017-02-23 20:49:18 +0100

    Implemented Class to part slave sub map

--- src/main/java/appeng/core/me/AppEngME.java
+++ src/main/java/appeng/core/me/AppEngME.java
@@ -3,6 +3,10 @@ package appeng.core.me;
 
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.google.common.collect.BiMap;
 
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
@@ -16,6 +20,11 @@ import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 import net.minecraftforge.fml.common.event.FMLServerStoppedEvent;
 import net.minecraftforge.fml.common.event.FMLServerStoppingEvent;
 import net.minecraftforge.fml.common.registry.FMLControlledNamespacedRegistry;
+import net.minecraftforge.fml.common.registry.IForgeRegistry;
+import net.minecraftforge.fml.common.registry.IForgeRegistry.AddCallback;
+import net.minecraftforge.fml.common.registry.IForgeRegistry.ClearCallback;
+import net.minecraftforge.fml.common.registry.IForgeRegistry.CreateCallback;
+import net.minecraftforge.fml.common.registry.IForgeRegistry.SubstitutionCallback;
 import net.minecraftforge.fml.common.registry.RegistryBuilder;
 
 import appeng.api.definitions.IDefinition;
@@ -30,6 +39,7 @@ import appeng.core.lib.bootstrap.FeatureFactory;
 import appeng.core.lib.features.AEFeature;
 import appeng.core.me.api.IME;
 import appeng.core.me.api.part.PartRegistryEntry;
+import appeng.core.me.api.parts.IPart;
 import appeng.core.me.definitions.MEBlockDefinitions;
 import appeng.core.me.definitions.MEItemDefinitions;
 import appeng.core.me.definitions.METileDefinitions;
@@ -41,6 +51,8 @@ public class AppEngME implements IME
 
 	@Module.Instance( NAME )
 	public static final AppEngME INSTANCE = null;
+
+	private static final ResourceLocation CLASS2PARTMAP = new ResourceLocation( AppEng.MODID, "class-partrege_map" );
 
 	private FMLControlledNamespacedRegistry<PartRegistryEntry<?>> partRegistry;
 
@@ -73,10 +85,23 @@ public class AppEngME implements IME
 		return partRegistry;
 	}
 
+	private Map<Class<?>, ResourceLocation> getClass2PartMap( Map<ResourceLocation, ?> slaveset )
+	{
+		return (Map<Class<?>, ResourceLocation>) slaveset.get( CLASS2PARTMAP );
+	}
 	@ModuleEventHandler
 	public void preInit( FMLPreInitializationEvent event )
 	{
-		partRegistry = (FMLControlledNamespacedRegistry<PartRegistryEntry<?>>) new RegistryBuilder().setName( new ResourceLocation( AppEng.MODID, "parts" ) ).setType( PartRegistryEntry.class ).create();
+		// @formatter:off
+		partRegistry = (FMLControlledNamespacedRegistry<PartRegistryEntry<?>>) new RegistryBuilder().setName( new ResourceLocation( AppEng.MODID, "parts" ) ).setType( PartRegistryEntry.class )
+				.add( (CreateCallback<PartRegistryEntry<?>>) ( ( Map<ResourceLocation, ?> slaveset, BiMap<ResourceLocation, ? extends IForgeRegistry<?>> registries ) -> ( (Map) slaveset ).put( CLASS2PARTMAP, new HashMap() ) ) )
+				.add( (AddCallback<PartRegistryEntry<?>>) ( ( PartRegistryEntry<?> part, int id, Map<ResourceLocation, ?> slaveset ) -> getClass2PartMap( slaveset ).put( part.getPartClass(), part.getRegistryName() ) ) )
+				.add( (ClearCallback<PartRegistryEntry<?>>) ( ( IForgeRegistry<PartRegistryEntry<?>> is, Map<ResourceLocation, ?> slaveset ) -> getClass2PartMap( slaveset ).clear() ) )
+				.add( (SubstitutionCallback<PartRegistryEntry<?>>) ( ( Map<ResourceLocation, ?> slaveset, PartRegistryEntry<?> original, PartRegistryEntry<?> replacement, ResourceLocation name ) -> {
+					getClass2PartMap( slaveset ).remove( original.getClass() );
+					getClass2PartMap( slaveset ).put( replacement.getClass(), name );
+				} ) ).create();
+		// @formatter:on
 
 		registry = new FeatureFactory();
 		this.blockDefinitions = new MEBlockDefinitions( registry );
