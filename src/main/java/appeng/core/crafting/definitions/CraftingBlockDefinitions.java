60aa3bf7b8fa95fd2aa66e7da47bc48346877469
elix-x <Elix-x@users.noreply.github.com>
2017-02-28 11:17:14 +0100

    Moved crafting item def classes (to crafting)

--- src/main/java/appeng/core/crafting/definitions/CraftingBlockDefinitions.java
+++ src/main/java/appeng/core/crafting/definitions/CraftingBlockDefinitions.java
@@ -12,7 +12,7 @@ import appeng.core.crafting.block.BlockCraftingMonitor;
 import appeng.core.crafting.block.BlockCraftingStorage;
 import appeng.core.crafting.block.BlockCraftingUnit;
 import appeng.core.crafting.block.BlockMolecularAssembler;
-import appeng.core.crafting.block.ItemCraftingStorage;
+import appeng.core.crafting.item.ItemCraftingStorage;
 import appeng.core.lib.bootstrap.FeatureFactory;
 import appeng.core.lib.definitions.Definitions;
 import appeng.core.lib.features.AEFeature;
