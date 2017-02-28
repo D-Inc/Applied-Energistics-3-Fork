9d7d147dc582f192e16ccf2cf1c46e22552aa5f1
elix-x <Elix-x@users.noreply.github.com>
2017-02-28 11:39:53 +0100

    Moved world gen def classes (to world gen)

--- src/main/java/appeng/miscellaneous/definitions/MiscellaneousBlockDefinitions.java
+++ src/main/java/appeng/miscellaneous/definitions/MiscellaneousBlockDefinitions.java
@@ -12,11 +12,11 @@ import appeng.core.block.BlockTinyTNT;
 import appeng.core.lib.bootstrap.FeatureFactory;
 import appeng.core.lib.definitions.Definitions;
 import appeng.core.lib.features.AEFeature;
+import appeng.core.worldgen.block.BlockSkyCompass;
 import appeng.miscellaneous.AppEngMiscellaneous;
 import appeng.miscellaneous.api.definitions.IMiscellaneousBlockDefinitions;
 import appeng.miscellaneous.block.BlockLightDetector;
 import appeng.miscellaneous.block.BlockPaint;
-import appeng.miscellaneous.block.BlockSkyCompass;
 import appeng.tools.hooks.DispenserBehaviorTinyTNT;
 
 
