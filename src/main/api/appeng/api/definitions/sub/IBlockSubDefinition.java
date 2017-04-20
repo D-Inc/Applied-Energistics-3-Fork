4c89c852eac217485d20b1fee6803d553909f320
elix-x <Elix-x@users.noreply.github.com>
2017-04-20 21:27:06 +0200

    Fixed blocksubdef format

--- src/main/api/appeng/api/definitions/sub/IBlockSubDefinition.java
+++ src/main/api/appeng/api/definitions/sub/IBlockSubDefinition.java
@@ -1,7 +1,10 @@
+
 package appeng.api.definitions.sub;
+
 
 import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
+
 
 public interface IBlockSubDefinition<BS extends IBlockState, B extends Block, S extends IBlockSubDefinition<BS, B, S>> extends ISubDefinition<BS, B, S>
 {
