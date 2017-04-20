0249a0fd2ee9a8a5ba3c02b72b3cee1ce52f7ef3
elix-x <Elix-x@users.noreply.github.com>
2017-04-20 22:22:49 +0200

    Made iitemmaterial explicitly extend state item
    Just because.
    ...maybe also for serialization purposes of API users.

--- src/main/api/appeng/core/api/items/IItemMaterial.java
+++ src/main/api/appeng/core/api/items/IItemMaterial.java
@@ -4,8 +4,10 @@ package appeng.core.api.items;
 
 import net.minecraft.item.Item;
 
+import appeng.api.item.IStateItem;
 
-public interface IItemMaterial<I extends Item & IItemMaterial<I>>
+
+public interface IItemMaterial<I extends Item & IItemMaterial<I>> extends IStateItem<I>
 {
 
 }
