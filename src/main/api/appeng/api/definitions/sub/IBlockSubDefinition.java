b568ab145771ce11a0b644b6be32f4942d280ac3
elix-x <Elix-x@users.noreply.github.com>
2017-04-20 21:58:54 +0200

    Created default item & block subdef ifaces

--- src/main/api/appeng/api/definitions/sub/IBlockSubDefinition.java
+++ src/main/api/appeng/api/definitions/sub/IBlockSubDefinition.java
@@ -6,7 +6,15 @@ import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
 
 
-public interface IBlockSubDefinition<BS extends IBlockState, B extends Block, S extends IBlockSubDefinition<BS, B, S>> extends ISubDefinition<BS, B, S>
+/**
+ * Default interface for block sub-definitions. For use with type parameters. All AE block sub definitions implement this.
+ * 
+ * @author Elix_x
+ *
+ * @param <S> IBlockState container
+ * @param <B> Block
+ */
+public interface IBlockSubDefinition<S extends IBlockState, B extends Block> extends ISubDefinition<S, B, IBlockSubDefinition<S, B>>
 {
 
 }
